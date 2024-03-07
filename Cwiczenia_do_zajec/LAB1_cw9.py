9b915e07661a461fcf479c72cfb0c436ad993d1c
gabost1023 <161207635+gabost1023@users.noreply.github.com>
2024-03-07 19:03:11 +0100

    cw9

---
+++ Cwiczenia_do_zajec/LAB1_cw9.py
@@ -0,0 +1,8 @@
+text = "bla bla"
+fl = 1.23451
+heksa = 0xDEADBEEF
+
+print("Tekst: {:s}".format(text))
+print("Liczba zmiennoprzecinkowa: {:f}".format(fl))
+print("Liczba szesnastkowa: 0x{:X}".format(heksa))
+
