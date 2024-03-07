fe95a9be35037b8cfec3e78906960c9479c4ba50
gabost1023 <161207635+gabost1023@users.noreply.github.com>
2024-03-07 18:06:15 +0100

    cw1

--- Cwiczenia_do_zajec/LAB1_cw1.py
+++ Cwiczenia_do_zajec/LAB1_cw1.py
@@ -1,1 +1,10 @@
-grsddrhs
+string1 = "Hello World!"
+string2 = "kot"
+int1 = 5
+int2 = 3
+float1 = 3.5
+float2 = 1.35
+comp1 = 3+2j
+comp2 = 2+3j
+
+print(string1,string2,int1,int2,float1,float2,comp1,comp2)
