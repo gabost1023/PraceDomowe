331c94eee6014860afd0088d7e207afa7ec7f575
gabost1023 <161207635+gabost1023@users.noreply.github.com>
2024-03-07 18:33:00 +0100

    cw4

---
+++ Cwiczenia_do_zajec/LAB1_cw4.py
@@ -0,0 +1,15 @@
+import math
+
+e = 2.71828182846
+print("e^10 = ", pow(e, 10))
+
+a = 5 + math.sin(8)**2
+logarytm = math.log(a)
+
+print("Pierwiastek 6. stopnia z ln(5 + sin^2(8)) wynosi: ", logarytm ** (1/6))
+
+liczba1 = math.floor(3.55)
+liczba2 = math.ceil(4.80)
+
+print("⌊3,55⌋ wynosi: ", liczba1)
+print("⌈4,80⌉ wynosi: ", liczba2)
