e4536f4278a9b5371b057cb82ed7334ba7ee083a
gabost1023 <161207635+gabost1023@users.noreply.github.com>
2024-03-08 16:39:22 +0100

    cw1

---
+++ Cwiczenia_do_zajec/LAB2_cw1.py
@@ -0,0 +1,7 @@
+sporty = ["silownia","siatkowka","pilka nozna","pilka reczna"]
+print(sporty)
+sporty.reverse()
+print(sporty)
+sporty.append("footbal amerykanski")
+sporty.append("taniec")
+print(sporty)
