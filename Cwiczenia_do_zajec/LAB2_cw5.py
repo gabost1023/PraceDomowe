5057e8f2f8d12ab6808c299417aec3cc42100cbb
gabost1023 <161207635+gabost1023@users.noreply.github.com>
2024-03-08 16:58:27 +0100

    cw5

---
+++ Cwiczenia_do_zajec/LAB2_cw5.py
@@ -0,0 +1,11 @@
+import sys as system
+
+system.stdout.write("Podaj a:")
+a = int(system.stdin.readline())
+system.stdout.write("Podaj b:")
+b = int(system.stdin.readline())
+system.stdout.write("Podaj c:")
+c = int(system.stdin.readline())
+wynik = pow(a, b) + c
+wynik = str(wynik)
+system.stdout.write(wynik)
