fc86e566183d892b634b68d64e49b7fc68d72fbd
gabost1023 <161207635+gabost1023@users.noreply.github.com>
2024-03-28 18:50:12 +0100

    cw1

---
+++ Kolokwium 1/Kol1.py
@@ -0,0 +1,21 @@
+import math
+
+liczba1 = pow(math.exp(4)-math.log(34,2), 1/3)
+
+print(round(liczba1, 2))
+
+liczba2 = pow(math.log(20) + math.cos(math.radians(45)) + math.exp(1), 1/3)
+
+print(round(liczba2, 2))
+
+liczba3 = pow(math.log(20,3) + (math.sin(math.radians(45)) * (5/8)), 1/4)
+
+print(round(liczba3, 2))
+
+liczba4 = math.log(23, 3) + pow(math.sin(math.radians(34))+5, 1/3)
+
+print(round(liczba4, 2))
+
+liczba5 = pow(math.log(32, 2) + math.pi + math.sin(math.radians(56)), 1/4)
+
+print(round(liczba5, 2))
